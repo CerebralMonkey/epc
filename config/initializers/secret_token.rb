3fdb872e128ebce283a83809e85accabb8709e6b
CerebralMonkey <cerebralmonkey@hotmail.com>
2013-01-23 20:32:51 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EchoWebsite::Application.config.secret_token = '27a17ae141c291014d9e313931358f4bc03804afb9ca7d7d912475f60eb864e0b09dd763ac2b2b0c2608537edc3023ef14a7d766b431b7b6bc8e947b8ed15992'
